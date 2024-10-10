49a74f19aa40288900190cbfa56ddd5c94f42a4e
Gio <44237862+Redifexx@users.noreply.github.com>
2024-10-09 22:43:21 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadddd.
+          Giovanni Perez Colon
         </p>
         <a
           className="App-link"
